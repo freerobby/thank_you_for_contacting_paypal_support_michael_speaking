ee86b19f84ca3a0931b3ed728905efe3fe02fbcb
Robby Grossman <robby@freerobby.com>
2011-05-04 22:45:31 -0400

    Loop 4 at a time, infinitely with prompt approval between runs.

--- bot.rb
+++ bot.rb
@@ -4,8 +4,6 @@ require "bundler/setup"
 require "tweetstream"
 require "twitter"
 
-hit_count = 0
-
 NEED_HELP_PHRASES = [
   'difficult',
   'dispute',
@@ -33,35 +31,44 @@ VICTIM = ENV['VICTIM']
 
 tweets = []
 
-start_time = Time.now
-puts "Beginning joke at #{start_time}"
-TweetStream::Client.new(ENV['TWITTER_BOT_USERNAME'], ENV['TWITTER_BOT_PASSWORD']).track('paypal') do |status, client|
-  found_a_hit = false
+continue = true
+
+while (continue) do
+  start_time = Time.now
+  puts "Beginning joke at #{start_time}"
+  hit_count = 0
+  TweetStream::Client.new(ENV['TWITTER_BOT_USERNAME'], ENV['TWITTER_BOT_PASSWORD']).track('paypal') do |status, client|
+    found_a_hit = false
   
-  search_text = status.text.downcase
-  NEED_HELP_PHRASES.map do |phrase|
-    found_a_hit = true if search_text.include?(phrase)
+    search_text = status.text.downcase
+    NEED_HELP_PHRASES.map do |phrase|
+      found_a_hit = true if search_text.include?(phrase)
+    end
+  
+    if found_a_hit
+      hit_count += 1
+      puts "HIT: #{status.user.screen_name} just wrote: \"#{status.text}\""
+      str = "@#{status.user.screen_name} Sorry you're having trouble with PayPal. Please contact @#{VICTIM}, our friendly support rep, for help."
+      tweets << {:text => str, :reply_to_id => status.id}
+      puts "Tweet ##{hit_count} prepared."
+    else
+      puts "miss: #{status.user.screen_name} just wrote: \"#{status.text}\""
+    end
+  
+    client.stop if hit_count >= 4
   end
-  
-  if found_a_hit
-    hit_count += 1
-    puts "HIT: #{status.user.screen_name} just wrote: \"#{status.text}\""
-    str = "@#{status.user.screen_name} Sorry you're having trouble with PayPal. Please contact @#{VICTIM}, our friendly support rep, for help."
-    puts "Preparing tweet: #{str}"
-    tweets << {:text => str, :reply_to_id => status.id}
-  else
-    puts "miss: #{status.user.screen_name} just wrote: \"#{status.text}\""
+  puts "Sending tweets!"
+  tweets.each do |tweet|
+    puts tweet[:reply_to_id]
+    x = Twitter.update(tweet[:text], {:in_reply_to_status_id => tweet[:reply_to_id]})
   end
+  tweets.clear
+  puts "Tweets sent!"
+  end_time = Time.now
+  puts "#{hit_count} jokes played on #{VICTIM} by #{end_time}"
+  puts "Elapsed time: #{end_time - start_time} seconds."
   
-  client.stop if hit_count >= 5
+  puts ""
+  puts "Enter 'y' + [Return] to continue"
+  continue = gets.chomp == 'y'
 end
-puts "Sending tweets!"
-tweets.each do |tweet|
-  puts tweet[:reply_to_id]
-  x = Twitter.update(tweet[:text], {:in_reply_to_status_id => tweet[:reply_to_id]})
-  puts x
-end
-puts "Tweets sent!"
-end_time = Time.now
-puts "#{hit_count} jokes played on #{VICTIM} by #{end_time}"
-puts "Elapsed time: #{end_time - start_time} seconds."
