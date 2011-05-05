ad2b07deaf72740215341d34b0d8ad99c024b002
Robby Grossman <robby@freerobby.com>
2011-05-04 21:38:40 -0400

    It tweets!

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 
 gem 'tweetstream', '1.0.4'
+gem 'twitter', '1.4.1'
--- bot.rb
+++ bot.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "bundler/setup"
 
 require "tweetstream"
+require "twitter"
 
 hit_count = 0
 
@@ -21,6 +22,15 @@ NEED_HELP_PHRASES = [
   'worst'
 ]
 
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_BOT_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_BOT_CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_BOT_OAUTH_KEY']
+  config.oauth_token_secret = ENV['TWITTER_BOT_OAUTH_SECRET']
+end
+
+VICTIM = ENV['VICTIM']
+
 start_time = Time.now
 puts "Beginning joke at #{start_time}"
 TweetStream::Client.new(ENV['TWITTER_BOT_USERNAME'], ENV['TWITTER_BOT_PASSWORD']).track('paypal') do |status, client|
@@ -34,12 +44,15 @@ TweetStream::Client.new(ENV['TWITTER_BOT_USERNAME'], ENV['TWITTER_BOT_PASSWORD']
   if found_a_hit
     hit_count += 1
     puts "HIT: #{status.user.screen_name} just wrote: \"#{status.text}\""
+    str = "@#{status.user.screen_name} Sorry you're having trouble. For prompt service, please contact @#{VICTIM}, our friendly support rep."
+    Twitter.update(str)
+    puts "TWEETED: #{str}"
   else
     puts "miss: #{status.user.screen_name} just wrote: \"#{status.text}\""
   end
   
-  client.stop if hit_count >= 3
+  client.stop if hit_count >= 5
 end
 end_time = Time.now
-puts "#{hit_count} jokes played on macasek by #{end_time}"
+puts "#{hit_count} jokes played on #{VICTIM} by #{end_time}"
 puts "Elapsed time: #{start_time - end_time} seconds."
